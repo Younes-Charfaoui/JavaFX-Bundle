3defebef546808ddb1c8074feb7c92b891e67692
Younes Charfaoui <mxcsyounes@gmail.com>
2019-06-30 17:29:43 +0100

    Testing the newly created class, need lot of improvments

---
+++ src/test/java/me/mxcsyoues/fxbundle/FXMLBundleLoaderTest.java
@@ -0,0 +1,33 @@
+package me.mxcsyoues.fxbundle;
+
+import javafx.scene.Parent;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.io.IOException;
+
+@RunWith(MockitoJUnitRunner.class)
+public class FXMLBundleLoaderTest {
+
+    @Test
+    public void addExtra() throws IOException {
+        FXMLBundleLoader loader = new FXMLBundleLoader(getClass().getResource(""));
+        loader.getBundle();
+        Assert.assertEquals(loader.getBundle().size(), 0);
+        loader.addExtra("String", "String")
+                .addExtra("int", 1)
+                .addExtra("double", 2.0)
+                .addExtra("Object", new StringBuilder())
+                .addExtra("long", 2L);
+        Assert.assertEquals(loader.getBundle().size(), 5);
+        Assert.assertEquals(loader.getBundle().getStringExtra("String", ""), "String");
+    }
+
+    @Test
+    public void buildAndLoad() throws IOException {
+        FXMLBundleLoader loader = new FXMLBundleLoader(getClass().getResource(""));
+        Parent parent = loader.buildAndLoad();
+    }
+}
